f092cedee1922410302ab532fa6c274a5b61b190
Waqar ahmad <waqar.ah.awan@gmail.com>
2026-03-17 10:02:50 +0400

    Compose.yml + grafana

--- src/main/java/com/waqar/ship/shorturl/ShortUrlController.java
+++ src/main/java/com/waqar/ship/shorturl/ShortUrlController.java
@@ -1,17 +1,18 @@
 package com.waqar.ship.shorturl;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import io.micrometer.observation.annotation.Observed;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import com.waqar.ship.shorturl.entity.ShortUrl;
 
 
 @RequestMapping("api/{version}/url")
 @RestController
 public class ShortUrlController {
+    private static final Logger log = LoggerFactory.getLogger(ShortUrlController.class);
 
     private final ShortUrlService shortUrlService;
 
@@ -26,13 +27,16 @@ public class ShortUrlController {
     }
 
     @GetMapping(value = "/{shortUrl}")
+    @Observed(name ="getUrl.count")
     public ResponseEntity<String> getUrl(@PathVariable String shortUrl) throws InterruptedException {
+        log.info("Controller getUrl started");
         ShortUrlResponse longUrl = shortUrlService.getLongUrl(shortUrl);
 
         if("".equalsIgnoreCase(longUrl.getLongUrl())){
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("");
         }
-        
+
+        log.info("Controller getUrl ended");
         return ResponseEntity.status(HttpStatus.MOVED_PERMANENTLY)
             .header("Location", longUrl.getLongUrl())
             .build();
